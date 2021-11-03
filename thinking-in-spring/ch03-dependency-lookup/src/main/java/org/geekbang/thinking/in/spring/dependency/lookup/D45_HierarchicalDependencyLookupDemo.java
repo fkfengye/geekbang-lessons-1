817317170cb61f7c10ced9e0a1c8dafd2fcd94bd
fankai <1778770286@qq.com>
2021-11-03 10:18:31 +0800

    依赖注入

--- thinking-in-spring/ch03-dependency-lookup/src/main/java/org/geekbang/thinking/in/spring/dependency/lookup/D45_HierarchicalDependencyLookupDemo.java
+++ thinking-in-spring/ch03-dependency-lookup/src/main/java/org/geekbang/thinking/in/spring/dependency/lookup/D45_HierarchicalDependencyLookupDemo.java
@@ -24,28 +24,28 @@ import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 /**
+ * 45、层次性依赖查找：依赖查找也有双亲委派？
  * 层次性依赖查找示例
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since
  */
-public class HierarchicalDependencyLookupDemo {
+public class D45_HierarchicalDependencyLookupDemo {
 
     public static void main(String[] args) {
         // 创建 BeanFactory 容器
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        // 将当前类 ObjectProviderDemo 作为配置类（Configuration Class）
-        applicationContext.register(ObjectProviderDemo.class);
+        // 将当前类 D46_ObjectProviderDemo 作为配置类（Configuration Class）
+        applicationContext.register(D46_ObjectProviderDemo.class);
 
-        // 1. 获取 HierarchicalBeanFactory <- ConfigurableBeanFactory <- ConfigurableListableBeanFactory
+        // 1、获取 HierarchicalBeanFactory <- ConfigurableBeanFactory <- ConfigurableListableBeanFactory
         ConfigurableListableBeanFactory beanFactory = applicationContext.getBeanFactory();
-//        System.out.println("当前 BeanFactory 的 Parent BeanFactory ： " + beanFactory.getParentBeanFactory());
 
-        // 2. 设置 Parent BeanFactory
+        // 2、设置 Parent BeanFactory
         HierarchicalBeanFactory parentBeanFactory = createParentBeanFactory();
         beanFactory.setParentBeanFactory(parentBeanFactory);
-//        System.out.println("当前 BeanFactory 的 Parent BeanFactory ： " + beanFactory.getParentBeanFactory());
 
+        // 3、通过containsLocalBean方法 测试本地BeanFactory中是存在Bean
         displayContainsLocalBean(beanFactory, "user");
         displayContainsLocalBean(parentBeanFactory, "user");
 
@@ -60,11 +60,50 @@ public class HierarchicalDependencyLookupDemo {
 
     }
 
-    private static void displayContainsBean(HierarchicalBeanFactory beanFactory, String beanName) {
-        System.out.printf("当前 BeanFactory[%s] 是否包含 Bean[name : %s] : %s\n", beanFactory, beanName,
-                containsBean(beanFactory, beanName));
+
+    /**
+     * 1、随便创建一个 parent BeanFactory
+     *      我们知道，这个Bean里面存在User实例
+     * @return
+     */
+    private static ConfigurableListableBeanFactory createParentBeanFactory() {
+        // 创建 BeanFactory 容器
+        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
+        // XML 配置文件 ClassPath 路径
+        String location = "classpath:/META-INF/dependency-lookup-context.xml";
+        // 加载配置
+        reader.loadBeanDefinitions(location);
+        return beanFactory;
     }
 
+
+    /**
+     * 2、采用 containsLocalBean 判断当前beanFactory中是否包含某个Bean
+     * @param beanFactory
+     * @param beanName
+     */
+    private static void displayContainsLocalBean(HierarchicalBeanFactory beanFactory, String beanName) {
+        System.out.printf("当前 BeanFactory[%s] 是否包含 Local Bean[name : %s] : %s\n",
+                beanFactory, beanName, beanFactory.containsLocalBean(beanName));
+    }
+
+    /**
+     * 3、通过层次性，逐层递归 beanFactory 判断是否存在某个Bean
+     * @param beanFactory
+     * @param beanName
+     */
+    private static void displayContainsBean(HierarchicalBeanFactory beanFactory, String beanName) {
+        System.out.printf("当前 BeanFactory[%s] 是否包含 Bean[name : %s] : %s\n",
+                beanFactory, beanName, containsBean(beanFactory, beanName));
+    }
+
+    /**
+     * 4、递归查找Bean
+     * @param beanFactory
+     * @param beanName
+     * @return
+     */
     private static boolean containsBean(HierarchicalBeanFactory beanFactory, String beanName) {
         BeanFactory parentBeanFactory = beanFactory.getParentBeanFactory();
         if (parentBeanFactory instanceof HierarchicalBeanFactory) {
@@ -76,20 +115,5 @@ public class HierarchicalDependencyLookupDemo {
         return beanFactory.containsLocalBean(beanName);
     }
 
-    private static void displayContainsLocalBean(HierarchicalBeanFactory beanFactory, String beanName) {
-        System.out.printf("当前 BeanFactory[%s] 是否包含 Local Bean[name : %s] : %s\n", beanFactory, beanName,
-                beanFactory.containsLocalBean(beanName));
-    }
-
-    private static ConfigurableListableBeanFactory createParentBeanFactory() {
-        // 创建 BeanFactory 容器
-        DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
-        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(beanFactory);
-        // XML 配置文件 ClassPath 路径
-        String location = "classpath:/META-INF/dependency-lookup-context.xml";
-        // 加载配置
-        reader.loadBeanDefinitions(location);
-        return beanFactory;
-    }
 
 }
--- thinking-in-spring/ch03-dependency-lookup/src/main/java/org/geekbang/thinking/in/spring/dependency/lookup/D46_ObjectProviderDemo.java
+++ thinking-in-spring/ch03-dependency-lookup/src/main/java/org/geekbang/thinking/in/spring/dependency/lookup/D46_ObjectProviderDemo.java
@@ -23,23 +23,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
 
 /**
+ * 46、延迟依赖查找：费延迟初始化Bean也能实现延迟查找？
+ *
  * 通过 {@link ObjectProvider} 进行依赖查找
  *
  * @author <a href="mailto:mercyblitz@gmail.com">Mercy</a>
  * @since
  */
-public class ObjectProviderDemo { // @Configuration 是非必须注解
+public class D46_ObjectProviderDemo { // @Configuration 是非必须注解
 
     public static void main(String[] args) {
         // 创建 BeanFactory 容器
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
-        // 将当前类 ObjectProviderDemo 作为配置类（Configuration Class）
-        applicationContext.register(ObjectProviderDemo.class);
+        // 将当前类 D46_ObjectProviderDemo 作为配置类（Configuration Class）
+        applicationContext.register(D46_ObjectProviderDemo.class);
         // 启动应用上下文
         applicationContext.refresh();
+
         // 依赖查找集合对象
+        System.out.println("--------------- lookupByObjectProvider -----------");
         lookupByObjectProvider(applicationContext);
+
+        System.out.println("\n--------------- lookupIfAvailable -----------");
         lookupIfAvailable(applicationContext);
+
+        System.out.println("\n--------------- lookupByStreamOps -----------");
         lookupByStreamOps(applicationContext);
 
         // 关闭应用上下文
@@ -47,25 +55,14 @@ public class ObjectProviderDemo { // @Configuration 是非必须注解
 
     }
 
-    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
-        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
-//        Iterable<String> stringIterable = objectProvider;
-//        for (String string : stringIterable) {
-//            System.out.println(string);
-//        }
-        // Stream -> Method reference
-        objectProvider.stream().forEach(System.out::println);
-    }
 
-    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
-        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
-        User user = userObjectProvider.getIfAvailable(User::createUser);
-        System.out.println("当前 User 对象：" + user);
-    }
-
+    /**
+     * 没有指定的情况下，方法名就是 Bean 名称 = "helloWorld"
+     * @return
+     */
     @Bean
     @Primary
-    public String helloWorld() { // 方法名就是 Bean 名称 = "helloWorld"
+    public String helloWorld() {
         return "Hello,World";
     }
 
@@ -74,8 +71,40 @@ public class ObjectProviderDemo { // @Configuration 是非必须注解
         return "Message";
     }
 
+
+    /**
+     * 1、ObjectProvider <- ObjectFactory、Iterable 的子类
+     *      采用 ObjectProvider、ObjectFactory 可以实现延迟加载
+     *
+     * 下面单一查找Bean
+     * @param applicationContext
+     */
     private static void lookupByObjectProvider(AnnotationConfigApplicationContext applicationContext) {
         ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
         System.out.println(objectProvider.getObject());
     }
+
+
+    /**
+     * 2、通过ObjectProvider延迟加载，查找一个不存在的User Bean对象
+     * @param applicationContext
+     */
+    private static void lookupIfAvailable(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<User> userObjectProvider = applicationContext.getBeanProvider(User.class);
+        // getIfAvailable 表示如果不存在，调用 User::createUser方法返回一个默认的User
+        User user = userObjectProvider.getIfAvailable(User::createUser);
+        System.out.println("当前 User 对象：" + user);
+    }
+
+
+    /**
+     * 3、通过ObjectProvider对象实现集合类型查找
+     * @param applicationContext
+     */
+    private static void lookupByStreamOps(AnnotationConfigApplicationContext applicationContext) {
+        ObjectProvider<String> objectProvider = applicationContext.getBeanProvider(String.class);
+        objectProvider.stream().forEach(System.out::println);
+    }
+
+
 }
